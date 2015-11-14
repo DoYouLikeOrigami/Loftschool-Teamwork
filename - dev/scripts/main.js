b8dfb33586814755b38afb407a70ead746652fbd
master-booms <forex1987@gmail.com>
2015-11-14 03:00:10 +0200

    createwatermark relize4 fix clone img

--- - dev/scripts/main.js
+++ - dev/scripts/main.js
@@ -14,53 +14,54 @@ var team9WatermarkGeneratorModule = (function () {
             acceptFileTypes: /(\.|\/)(gif|jpe?g|png)$/i,
             disableImageResize: /Android(?!.*Chrome)|Opera/
                 .test(window.navigator && navigator.userAgent),
-        url: 'server/php/',
-        add: function(e, data) {
-            console.log('add');
-            data.submit();
-        },
-        done: function(e, data) {
-             var  uploadImg = data.result.files[0];
-             if ($('.wrapрer__img').length > 0) {
-                    $('.img-upload').remove();
-                } else {
-                    // создаем обертку для изображения
-                    // $(".main__content").html("<div class='wrapper__img'></div>");
-                    // $(".wrapper__img").html("<div class='wrapper__img-resize'></div>");
-                }
-            // создаем наше изображения(загружаем его на сервер)
-            var img = $('<img></img>');
-            img.attr('src', uploadImg.url);
-            img.addClass('img-upload');
-            img.fadeIn('.wrapper__img-resize');
-            img.appendTo('.wrapper__img-resize');
-            img.load(function () {
-                    // получаем  цифры размера изображения из дополнительных классов
-                    var width = $(this).width(),
-                        height = $(this).height(),
-                        sizeHeight = $('.main__content').height(),
-                        sizeWidth = $('.main__content').width(),
-                        sizeBox = sizeWidth / sizeHeight,
-                        setResize = function (cssResize, heightR, widthR) {
-                            img.addClass(cssResize);
-                            $('.wrapper__img-resize').css({
-                                'height': heightR + 'px',
-                                'width': widthR + 'px'
-                            });
-                        };
-                         
-                    // и масштабируем его добавочным классом
-                    if ((width < sizeWidth) && (height < sizeHeight)) {
-                        setResize('', height, width);
-                    } else if (sizeBox < width / height) {
-                        setResize('img-upload-widthR ', Math.round(sizeWidth * height / width), sizeWidth);
+            url: 'server/php/',
+            add: function(e, data) {
+                console.log('add');
+                data.submit();
+            },
+            done: function(e, data) {
+                 var  uploadImg = data.result.files[0];
+                 if ($('.wrapрer__img').length > 0) {
+                        $('.img-upload').remove();
                     } else {
-                        setResize('img-upload-heightR ', sizeHeight, Math.round(sizeHeight * width / height));
+                        // создаем обертку для изображения
+                        // $(".main__content").html("<div class='wrapper__img'></div>");
+                        // $(".wrapper__img").html("<div class='wrapper__img-resize'></div>");
+                    }
+                // создаем наше изображения(загружаем его на сервер)
+                var img = $('<img></img>');
+                img.attr('src', uploadImg.url);
+                img.addClass('img-upload');
+                img.fadeIn('.wrapper__img-resize');
+                img.appendTo('.wrapper__img-resize');
+                img.load(function () {
+                        // получаем  цифры размера изображения из дополнительных классов
+                        var width = $(this).width(),
+                            height = $(this).height(),
+                            sizeHeight = $('.main__content').height(),
+                            sizeWidth = $('.main__content').width(),
+                            sizeBox = sizeWidth / sizeHeight,
+                            setResize = function (cssResize, heightR, widthR) {
+                                img.addClass(cssResize);
+                                $('.wrapper__img-resize').css({
+                                    'height': heightR + 'px',
+                                    'width': widthR + 'px'
+                                });
+                            };
+                             
+                        // и масштабируем его добавочным классом
+                        if ((width < sizeWidth) && (height < sizeHeight)) {
+                            setResize('', height, width);
+                        } else if (sizeBox < width / height) {
+                            setResize('img-upload-widthR ', Math.round(sizeWidth * height / width), sizeWidth);
+                        } else {
+                            setResize('img-upload-heightR ', sizeHeight, Math.round(sizeHeight * width / height));
                 }
             });
         }
     });
    //событие отправки вотермарка на сервер
+
      $('#water-file').fileupload({
             dataType: 'json',
             acceptFileTypes: /(\.|\/)(gif|jpe?g|png)$/i,
@@ -73,11 +74,14 @@ var team9WatermarkGeneratorModule = (function () {
             done: function (e, data) {
                 var uploadWtm = data.result.files[0];
                     var wrapper = $(".watermark__img-wrapper");
-                $(".wrapper__img-resize").append("<div class='watermark__img-wrapper'></div>");
+                // $(".wrapper__img-resize").append("<div class='watermark__img-wrapper'></div>");
+                
                     var imgWtm = $('<img></img>');
                         imgWtm.attr('src', uploadWtm.url);
                         imgWtm.addClass('img-watermark');
                         imgWtm.fadeIn('.watermark__img-wrapper');
+
+
                 $(".watermark__img-wrapper").html(imgWtm);
                  imgWtm.load(function () {
                     
@@ -101,7 +105,7 @@ var team9WatermarkGeneratorModule = (function () {
                         setResize('img-upload-widthR ', Math.round(sizeWidth * height / width), sizeWidth);
                     } else {
                         setResize('img-upload-heightR ', sizeHeight, Math.round(sizeHeight * width / height));
-                }
+                    }
             });
         }
     });
