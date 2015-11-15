f24ee96391defc0fd39d16f6e75d0853fc378ec4
Anton Nemtsev <doyoulikeorigami@gmail.com>
2015-11-15 16:38:56 +0300

    local commit

--- - dev/scripts/main.js
+++ - dev/scripts/main.js
@@ -10,96 +10,98 @@ var team9WatermarkGeneratorModule = (function () {
 
     
     // событие отправки изображения на сервер
-        $('#main-file').fileupload({
-            dataType: 'json',
-            acceptFileTypes: /(\.|\/)(gif|jpe?g|png)$/i,
-            disableImageResize: /Android(?!.*Chrome)|Opera/
-                .test(window.navigator && navigator.userAgent),
-            url: 'server/php/',
-            add: function(e, data) {
-                console.log('add');
-                data.submit();
-            },
-            done: function(e, data) {
-                 var  uploadImg = data.result.files[0],
-                      img = $('<img></img>');
-                // создаем наше изображения(загружаем его на сервер)
-                img.attr('src', uploadImg.url);
-                img.addClass('img-upload');
-                img.fadeIn('.wrapper__img-resize');
-                $('.wrapper__img-resize').html(img);
-                img.load(function () {
-                        // получаем  цифры размера изображения из дополнительных классов
-                        var width = $(this).width(),
-                            height = $(this).height(),
-                            sizeHeight = $('.main__content').height(),
-                            sizeWidth = $('.main__content').width(),
-                            sizeBox = sizeWidth / sizeHeight,
-                            setResize = function (cssResize, heightR, widthR) {
-                                img.addClass(cssResize);
-                                $('.wrapper__img-resize').css({
-                                    'height': heightR + 'px',
-                                    'width': widthR + 'px',
-                                    'margin-top' : - heightR / 2
-                                });
-                            };
-                             
-                        // и масштабируем его добавочным классом
-                        if ((width < sizeWidth) && (height < sizeHeight)) {
-                            setResize('', height, width);
-                        } else if (sizeBox < width / height) {
-                            setResize('img-upload-widthR ', Math.round(sizeWidth * height / width), sizeWidth);
-                        } else {
-                            setResize('img-upload-heightR ', sizeHeight, Math.round(sizeHeight * width / height));
-                }
-            });
+    $('#main-file').fileupload({
+      dataType: 'json',
+      acceptFileTypes: /(\.|\/)(gif|jpe?g|png)$/i,
+      disableImageResize: /Android(?!.*Chrome)|Opera/
+          .test(window.navigator && navigator.userAgent),
+      url: 'server/php/',
+
+      add: function(e, data) {
+          data.submit();
+      },
+
+      done: function(e, data) {
+        var uploadImg = data.result.files[0],
+            img = $('<img></img>');
+        // создаем наше изображения(загружаем его на сервер)
+        img.attr('src', uploadImg.url);
+        img.addClass('img-upload');
+        img.fadeIn('.wrapper__img-resize');
+        $('.wrapper__img-resize').html(img);
+        img.load(function () {
+          // получаем  цифры размера изображения из дополнительных классов
+          var width = $(this).width(),
+              height = $(this).height(),
+              sizeHeight = $('.main__content').height(),
+              sizeWidth = $('.main__content').width(),
+              sizeBox = sizeWidth / sizeHeight,
+              setResize = function (cssResize, heightR, widthR) {
+                img.addClass(cssResize);
+                $('.wrapper__img-resize').css({
+                  'height': heightR + 'px',
+                  'width': widthR + 'px',
+                  'margin-top' : - heightR / 2
+                });
+              };
+               
+          // и масштабируем его добавочным классом
+          if ((width < sizeWidth) && (height < sizeHeight)) {
+            setResize('', height, width);
+          } else if (sizeBox < width / height) {
+            setResize('img-upload-widthR ', Math.round(sizeWidth * height / width), sizeWidth);
+          } else {
+            setResize('img-upload-heightR ', sizeHeight, Math.round(sizeHeight * width / height));
           }
         });
+      }
+    });
      
    //событие отправки вотермарка на сервер
-     $('#water-file').fileupload({
-            dataType: 'json',
-            acceptFileTypes: /(\.|\/)(gif|jpe?g|png)$/i,
-            disableImageResize: /Android(?!.*Chrome)|Opera/
-            .test(window.navigator && navigator.userAgent),
-              url: 'server/php/',
-            add: function (e, data) { // отправляем картинку на сервер
-                data.submit();
-            },
-            done: function (e, data) {
-                var uploadWtm = data.result.files[0],
-                    WtmWrapper = $(".watermark__img-wrapper"),
-                    imgWtm = $('<img></img>');
-                    
-                    imgWtm.attr('src', uploadWtm.url);
-                    imgWtm.addClass('img-watermark');
-                    imgWtm.fadeIn('.watermark__img-wrapper');
-                    WtmWrapper.html(imgWtm);
-                    imgWtm.load(function () {
-                    
-                     // получаем  цифры размера изображения
-                    var width = $(this).width(),
-                        height = $(this).height(),
-                        sizeHeight =  $('.main__content').height(),
-                        sizeWidth =  $('.main__content').width(),
-                        sizeBox = sizeWidth / sizeHeight,
-                        setResize = function (cssResize, heightR, widthR) {
-                        imgWtm.addClass(cssResize);
-                        WtmWrapper.css({
-                            'height': heightR + 'px',
-                            'width': widthR + 'px'
-                        });
-                    };
-                    // и масштабируем его добавочным классом
-                    if ((width < sizeWidth) && (height < sizeHeight)) {
-                        setResize('', height, width);
-                    } else if (sizeBox < width / height) {
-                        setResize('img-upload-widthR ', Math.round(sizeWidth * height / width), sizeWidth);
-                    } else {
-                        setResize('img-upload-heightR ', sizeHeight, Math.round(sizeHeight * width / height));
-                    }
+    $('#water-file').fileupload({
+      dataType: 'json',
+      acceptFileTypes: /(\.|\/)(gif|jpe?g|png)$/i,
+      disableImageResize: /Android(?!.*Chrome)|Opera/
+      .test(window.navigator && navigator.userAgent),
+        url: 'server/php/',
+
+      add: function (e, data) { // отправляем картинку на сервер
+          data.submit();
+      },
+
+      done: function (e, data) {
+        var uploadWtm = data.result.files[0],
+            WtmWrapper = $(".watermark__img-wrapper"),
+            imgWtm = $('<img></img>');
+            
+            imgWtm.attr('src', uploadWtm.url);
+            imgWtm.addClass('img-watermark');
+            imgWtm.fadeIn('.watermark__img-wrapper');
+            WtmWrapper.html(imgWtm);
+            imgWtm.load(function () {
+              // получаем  цифры размера изображения
+              var width = $(this).width(),
+                  height = $(this).height(),
+                  sizeHeight =  $('.main__content').height(),
+                  sizeWidth =  $('.main__content').width(),
+                  sizeBox = sizeWidth / sizeHeight,
+                  setResize = function (cssResize, heightR, widthR) {
+                    imgWtm.addClass(cssResize);
+                    WtmWrapper.css({
+                      'height': heightR + 'px',
+                      'width': widthR + 'px'
+                    });
+                  };
+              // и масштабируем его добавочным классом
+              if ((width < sizeWidth) && (height < sizeHeight)) {
+                  setResize('', height, width);
+              } else if (sizeBox < width / height) {
+                  setResize('img-upload-widthR ', Math.round(sizeWidth * height / width), sizeWidth);
+              } else {
+                  setResize('img-upload-heightR ', sizeHeight, Math.round(sizeHeight * width / height));
+              }
             });
-        }
+      }
     });
   };
 
@@ -156,7 +158,7 @@ var team9WatermarkGeneratorModule = (function () {
   var _addTextToMainInput = function(e) {
     e.preventDefault();
 
-    console.log($(this));
+    //console.log($(this));
 
 
     var input = $(this),
